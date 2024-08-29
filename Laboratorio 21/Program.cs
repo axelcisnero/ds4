5d4d95cdaeea224a750a7883048eadd6d34928b8
axelcisnero <axel.cisnero@hotmail.com>
2024-08-28 22:18:18 -0500

    Se agrego el código del ejemplo 2 Laboratorio 21

---
+++ Laboratorio 21/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Laboratorio21
+{
+
+    public class Program
+    {
+        public static void main()
+        {
+            MyClass.Valor = 1;
+            Console.WriteLine(MyClass.Valor);
+        }
+    }
+    public class MyClass
+    {
+        public static int Valor;
+    }
+}
