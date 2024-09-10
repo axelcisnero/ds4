cd5013017552b85ea3d63c5c7867081ca9f2470d
axelcisnero <axel.cisnero@hotmail.com>
2024-09-09 19:17:48 -0500

    Laboratorio 42 terminado

--- Laboratorio 4/Laboratorio 42/Program.cs
+++ Laboratorio 4/Laboratorio 42/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EstructuraRepetitivaWhile
 {
-    private static void Main(string[] args)
+    class Program
     {
-        Console.WriteLine("Hello, World!");
+        static void Main(string[] args)
+        {
+            int fac=1,n;
+            string linea;
+            Console.Write("Ingrese un número entero: ");
+            linea = Console.ReadLine();
+            n = int.Parse(linea);
+            for (int i=1; i<=n; i++) {
+                fac = fac * i;
+            }
+                Console.Write("La factorial es: "+fac);
+                Console.ReadKey();
+        }
     }
 }
