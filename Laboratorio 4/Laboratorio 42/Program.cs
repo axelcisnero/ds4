ab6645403f12e546e1c1b3e089066cd38cbd8db9
axelcisnero <axel.cisnero@hotmail.com>
2024-09-09 19:36:42 -0500

    Se corrigen los nombres de los namespace de cada lab y se finaliza el lab44

--- Laboratorio 4/Laboratorio 42/Program.cs
+++ Laboratorio 4/Laboratorio 42/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace EstructuraRepetitivaWhile
+namespace Laboratorio4
 {
     class Program
     {
--- Laboratorio 4/Laboratorio 43/Program.cs
+++ Laboratorio 4/Laboratorio 43/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace EstructurarRepetitivaWhile
+namespace Laboratorio4
 {
     class Program
     {
