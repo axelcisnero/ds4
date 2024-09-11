6b34cd5270b450cb4c1502f467129106b115f578
axelcisnero <axel.cisnero@hotmail.com>
2024-09-11 18:46:53 -0500

    Creación y finalización de lab 51

---
+++ Laboratorio 5/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private int[] sueldos;
+
+    public void Cargar()
+    {
+        sueldos = new int[6];
+        for (int f = 1; f <= 5; f++)
+        {
+            Console.Write("Ingrese sueldo del operario "+f+": ");
+            String linea;
+            linea = Console.ReadLine();
+            sueldos[f] = int.Parse(linea);
+        }
+    }
+
+    public void Imprimir()
+    {
+        Console.Write("Los 5 sueldos de los operarios \n");
+        for (int f = 1; f <= 5; f++)
+        {
+            Console.Write("["+sueldos[f] + "]");
+        }
+        Console.ReadKey();
+    }
+
+    static void Main(string[] args)
+    {
+        PruebaVector1 pv = new PruebaVector1();
+        pv.Cargar();
+        pv.Imprimir();
+    }
+}
