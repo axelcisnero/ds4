d1d2b8e5707adca112ae381bd4ec4cb74dcca86e
axelcisnero <axel.cisnero@hotmail.com>
2024-09-18 19:25:57 -0500

    Creacion y finalización de laboratorio 6

---
+++ Laboratorio 6/Laboratorio 62/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num;
+        Console.WriteLine("Digite el numero deseado");
+
+        try
+        {
+            num = Int16.Parse(Console.ReadLine());
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("No ha introducido un dígito válido");
+            num = -1;
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("El número introducido es muy grande");
+            num = -1;
+        }
+
+        Console.WriteLine(num); // Esta línea ahora está dentro del bloque Main
+    }
+}
