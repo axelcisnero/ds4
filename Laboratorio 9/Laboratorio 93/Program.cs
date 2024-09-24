09410f36d5b1a3244b0b22783d6e4667b06da9b9
axelcisnero <axel.cisnero@hotmail.com>
2024-09-23 21:41:01 -0500

    Finalización problema 3 (93) Laboratorio 9

---
+++ Laboratorio 9/Laboratorio 93/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Laboratorio93
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el lado 1: ");
+        int lado1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el lado 2: ");
+        int lado2 = int.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el lado 3: ");
+        int lado3 = int.Parse(Console.ReadLine());
+
+        if (lado1 + lado2 > lado3 && lado1 + lado3 > lado2 && lado2 + lado3 > lado1)
+        {
+            if (lado1 == lado2 && lado2 == lado3)
+            {
+                Console.WriteLine("Es un triángulo equilátero.");
+            }
+            else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3)
+            {
+                Console.WriteLine("Es un triángulo isósceles.");
+            }
+            else
+            {
+                Console.WriteLine("Es un triángulo escaleno.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("No es un triángulo válido.");
+        }
+    }
+}
