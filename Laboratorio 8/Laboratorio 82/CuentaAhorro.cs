520b83bceb2883b01aabde59a4ca0e2c61bebb5e
axelcisnero <axel.cisnero@hotmail.com>
2024-09-23 22:22:27 -0500

    Creación y finalización laboratorio 82

---
+++ Laboratorio 8/Laboratorio 82/CuentaAhorro.cs
@@ -0,0 +1,12 @@
+﻿public class CuentaAhorro : Cuenta
+{
+    public CuentaAhorro(string prmtIdCuenta) : base(prmtIdCuenta)
+    {
+    }
+    public override void CalcularIntereses()
+    {
+        System.Console.WriteLine(
+        "CuentaAhorro.CalcularIntereses() efectuado para " +
+        "la cuenta {0}", getIdCuenta());
+    }
+}
