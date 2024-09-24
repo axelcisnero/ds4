ac92199665f8b07da0c3617a67c0fce977ba53fe
axelcisnero <axel.cisnero@hotmail.com>
2024-09-23 21:47:24 -0500

    Finalización problema 4 (94) Laboratorio 9

---
+++ Laboratorio 9/Laboratorio 94/Aleatorios.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Aleatorios
+{
+    private Random random = new Random();
+
+    public int GenerarNumero(int minimo, int maximo)
+    {
+        return random.Next(minimo, maximo + 1);
+    }
+
+    public int[] GenerarArreglo(int minimo, int maximo, int cantidad)
+    {
+        int[] arreglo = new int[cantidad];
+        for (int i = 0; i < cantidad; i++)
+        {
+            arreglo[i] = GenerarNumero(minimo, maximo);
+        }
+        return arreglo;
+    }
+}
---
+++ Laboratorio 9/Laboratorio 94/Program.cs
@@ -0,0 +1,16 @@
+﻿class Laboratorio94
+{
+    static void Main()
+    {
+        Aleatorios aleatorios = new Aleatorios();
+        int numero = aleatorios.GenerarNumero(1, 10);
+        Console.WriteLine("Número aleatorio: " + numero);
+
+        int[] arreglo = aleatorios.GenerarArreglo(1, 10, 5);
+        Console.WriteLine("Arreglo aleatorio:");
+        foreach (int num in arreglo)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
