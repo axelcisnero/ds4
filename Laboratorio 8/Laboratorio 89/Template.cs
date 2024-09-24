8836f849268b7a44d6db34c28ca08bf32c78efb1
axelcisnero <axel.cisnero@hotmail.com>
2024-09-24 15:27:44 -0500

    Creación y finalización de laboratorio 89

---
+++ Laboratorio 8/Laboratorio 89/Template.cs
@@ -0,0 +1,11 @@
+﻿class Template : iTemplate
+{
+    public void ponerVariable(string nombre, string var)
+    {
+        Console.WriteLine("Metodo poner variable {nombre} : {var}");
+    }
+    public void verHtml(string template)
+    {
+        Console.WriteLine(template);
+    }
+}
