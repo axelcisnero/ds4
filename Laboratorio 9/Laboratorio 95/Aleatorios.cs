97d163ef9f3977d205dbadeba825b13662a7e773
axelcisnero <axel.cisnero@hotmail.com>
2024-09-23 21:54:32 -0500

    Finalización problema 5 (95) Laboratorio 9

---
+++ Laboratorio 9/Laboratorio 95/Aleatorios.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Aleatorios
+{
+    private Random random = new Random();
+
+    public int GenerarNumero(int minimo, int maximo)
+    {
+        return random.Next(minimo, maximo + 1);
+    }
+
+    public int[] ArregloNoRepetido(int minimo, int maximo, int cantidad)
+    {
+        int[] arreglo = new int[cantidad];
+        int index = 0;
+
+        while (index < cantidad)
+        {
+            int num = GenerarNumero(minimo, maximo);
+            if (Array.IndexOf(arreglo, num) == -1)
+            {
+                arreglo[index] = num;
+                index++;
+            }
+        }
+
+        return arreglo;
+    }
+}
---
+++ Laboratorio 9/Laboratorio 95/Program.cs
@@ -0,0 +1,14 @@
+﻿class Laboratorio95
+{
+    static void Main()
+    {
+        Aleatorios aleatorios = new Aleatorios();
+        int[] arregloNoRepetido = aleatorios.ArregloNoRepetido(1, 10, 5);
+
+        Console.WriteLine("Arreglo sin números repetidos:");
+        foreach (int num in arregloNoRepetido)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
