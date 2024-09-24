eae6b6ba93716a4eb20885410ce9c4aa04308ee9
axelcisnero <axel.cisnero@hotmail.com>
2024-09-23 21:34:14 -0500

    Finalización problema 1 (91) Laboratorio 9

---
+++ Laboratorio 9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Laboratorio91
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el precio del producto (valor positivo): ");
+        float precio = float.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese la forma de pago (efectivo o tarjeta): ");
+        string formaPago = Console.ReadLine().ToLower();
+
+        if (formaPago == "tarjeta")
+        {
+            Console.Write("Ingrese el número de cuenta (16 dígitos): ");
+            string numeroCuenta = Console.ReadLine();
+            if (numeroCuenta.Length == 16)
+            {
+                Console.WriteLine("Pago procesado con tarjeta.");
+            }
+            else
+            {
+                Console.WriteLine("Número de cuenta inválido.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Pago procesado en efectivo.");
+        }
+    }
+}
