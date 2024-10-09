9e9d1251f19abb9ef592928b4e619fc0ef83a9dd
axelcisnero <axel.cisnero@hotmail.com>
2024-10-09 15:06:28 -0500

    Creacion y finalizacion de laboratorio 13

---
+++ Laboratorio 13/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Laboratorio_13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+       
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.productos_listbox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(219, 46);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(205, 90);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Conectar y desconectar de SQL Server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // productos_listbox
+            // 
+            this.productos_listbox.FormattingEnabled = true;
+            this.productos_listbox.ItemHeight = 16;
+            this.productos_listbox.Location = new System.Drawing.Point(178, 168);
+            this.productos_listbox.Name = "productos_listbox";
+            this.productos_listbox.Size = new System.Drawing.Size(290, 228);
+            this.productos_listbox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 450);
+            this.Controls.Add(this.productos_listbox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private ListBox productos_listbox;
+    }
+}
+
---
+++ Laboratorio 13/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Laboratorio_13
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Server=.\sqlexpress;Database=Northwind;TrustServerCertificate=true;Integrated Security=SSPI;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection(connectionString);
+
+            try
+            {
+                conexion.Open();
+                MessageBox.Show("Conexión abierta con éxito.");
+
+                // Consulta para obtener los nombres de los productos
+                string query = "SELECT ProductName FROM [dbo].[Products]";
+
+                SqlCommand command = new SqlCommand(query, conexion);
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Limpia el ListBox antes de llenarlo
+                productos_listbox.Items.Clear();
+
+                // Llena el ListBox con los nombres de los productos
+                while (reader.Read())
+                {
+                    productos_listbox.Items.Add(reader["ProductName"].ToString());
+                }
+
+                reader.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+            finally
+            {
+                if (conexion.State == ConnectionState.Open)
+                {
+                    conexion.Close();
+                    MessageBox.Show("Conexión cerrada.");
+                }
+            }
+        }
+    }
+}
